afeed8415f0248db6fc7b1b619f59312f600bb06
Artyom Trityak <art.trityak@gmail.com>
2016-09-14 13:39:06 -0700

    Some env variable

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 
 const app = express();
+app.set('view engine', 'ejs');
 app.use(express.static('static'));
+
+app.get('/', function(req, res) {
+  res.render('index', {SERVICE_URL: process.env.SERVICE_URL});
+});
 
 app.listen(5000, function () {
   console.log('App listening on port 5000...');
